b1037ec7d6313aa9edf327dcb2a2e75fa5441fe2
s1100560 <s1100560@mail.yzu.edu.tw>
2025-09-29 11:05:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify  # 添加 jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "OK"
+    return jsonify({"message": "OK"})  # 使用 jsonify
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
