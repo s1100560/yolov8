ab16d7b379b468b8b1bee6d4572856e133a1682d
s1100560 <s1100560@mail.yzu.edu.tw>
2025-10-01 19:42:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
+import os
+os.environ['YOLO_VERBOSE'] = 'False'
+os.environ['ULTRALYTICS_VERBOSE'] = 'False'
+
+# 加載模型時阻止自動下載
+from ultralytics import YOLO
+model = YOLO('freshness_fruit_and_vegetables.pt', verbose=False)
+
+
 from flask import Flask, jsonify
 
 app = Flask(__name__)
@@ -8,3 +17,4 @@ def home():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000, debug=False)
+
